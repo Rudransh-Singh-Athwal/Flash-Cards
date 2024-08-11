c58e3dd394c52ceb611b62d580319a7a664ce05f
Rudransh Singh Athwal <rsathwal04@gmail.com>
2024-08-11 12:31:05 +0530

    added some questions in array including id,question and answer

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,41 @@ export default function App() {
   );
 }
 
+const questions = [
+  {
+    id: 1,
+    question:
+      "How do you create a new project in React using Vite in terminal?",
+    answer: "Using npm create vite@latest flashcards",
+  },
+  {
+    id: 2,
+    question: "What are the building blocks of React apps?",
+    answer: "Components",
+  },
+  {
+    id: 3,
+    question: "What's the name of the syntax we use to describe a UI in React?",
+    answer: "JSX",
+  },
+  {
+    id: 4,
+    question: "How to pass data from parent to child components?",
+    answer: "Props",
+  },
+  {
+    id: 5,
+    question: "How to give components memory?",
+    answer: "useState hook",
+  },
+  {
+    id: 6,
+    question:
+      "What do we call an input element that is completely synchronised with state?",
+    answer: "Controlled element",
+  },
+];
+
 function FlashCards() {
   return (
     <div className="flashcards">
